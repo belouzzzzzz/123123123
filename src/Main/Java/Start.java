1fbd0b8792b7c5cd4e78c7117fa81b4298e35c7e
Belouzzz <belouzzzb52@gmail.com>
2022-11-22 21:00:22 +0300

    пробный коммит

---
+++ src/Main/Java/Start.java
@@ -0,0 +1,15 @@
+package Main.Java;
+
+public class Start {
+
+
+
+    public static void main (String[] args) {
+
+        for (int i=0; i<6; i++ ) {
+            System.out.println ("итерация" + i);
+        }
+
+
+    }
+}
